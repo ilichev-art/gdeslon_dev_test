0b686c598a18a50c195ec5f756e027ddc014657b
Artem Ilichev <artem.ilichev@list.ru>
2024-06-13 18:52:41 +0300

    add allure tags

--- tests/test_landingpage.py
+++ tests/test_landingpage.py
@@ -1,10 +1,16 @@
+from allure_commons.types import Severity
 from selene import browser
 import allure
 from models.landing_page import LandingPageMethods
 
 
-
 landing = LandingPageMethods()
+
+@allure.title('Поиск оффера')
+@allure.tag('web')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'Artem')
+@allure.feature(f'Работа поисковой строки в разделе Офферы')
 
 
 def test_search_offer(setup_browser):
